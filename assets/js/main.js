7aeedc7f408f8f92f60ab478fabed0bfe0b80590
Kaja <kajadekker@hotmail.com>
2018-05-22 10:44:38 +0200

    Added js to booking

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,15 @@ function carousel() {
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 4000); // Change image every 4 seconds
 }
+
+
+function addContact() {
+  var bookingName = document.getElementById("name").value;
+  var bookingCity = document.getElementById("city").value;
+  var bookingPhone = document.getElementById("phone").value;
+  var bookingMail = document.getElementById("mail").value;
+  var bookingDate = document.getElementById("date").value;
+  var bookingPersons = document.getElementById("persons").value
+  var booking = new Booking(bookingName, bookingCity, bookingPhone, bookingMail, bookingDate, bookingPersons);
+  
+}
